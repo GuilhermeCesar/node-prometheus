ccf4fc7008a1b59b4f3f17fc70eb1ad22b0b4cc7
Guilherme Medeiros <guilherme.medeiros@southsystem.com.br>
2021-05-08 14:18:50 -0300

    Adicionar todos os tipos de métricas

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-var express = require("express");
-var  prom = require('prom-client');
-const register = prom.register;
+var express = require('express');
+var promClient = require('prom-client');
+const register = promClient.register;
 
 var app = express();
 
-const counter = new prom.Counter({
-  name: 'aula_request_total',
-  help: 'Contador de request',
-  labelNames: ['statusCode']
+const counter = new promClient.Counter({
+	name: 'aula_request_total',
+	help: 'Contador de requests',
+	labelNames: ['statusCode']
 });
 
-const gauge = new prom.Gauge({
-  name: 'aula_free_bytes',
-  help: 'Exemplo de gauge',
-  labelNames: ['method', 'statusCode'],
+const gauge = new promClient.Gauge({
+	name: 'aula_free_bytes',
+	help: 'Exemplo de gauge'
 });
 
-app.get('/', function(req, rest) {
-   counter.labels('200').inc(); 
-   counter.labels('300').inc(); 
-   gauge.set(100*Math.random())
+const histogram = new promClient.Histogram({
+	name: 'aula_request_time_seconds',
+	help: 'Tempo de resposta da API',
+	buckets: [0.1, 0.2, 0.3, 0.4, 0.5]
+});
 
-  rest.send('Hello World meus amigos')  
-  
-})
+const summary = new promClient.Summary({
+	name: 'aula_summary_request_time_seconds',
+	help: 'Tempo de resposta da API',
+	percentiles: [0.5, 0.9, 0.99]
+});
 
-app.get('/metrics',async function(req, res) {
+app.get('/', function (req, res) {
+	counter.labels('200').inc();
+	counter.labels('300').inc();
+	gauge.set(100 * Math.random());
+	const tempo = Math.random();
+	histogram.observe(tempo);
+	summary.observe(tempo);
 
-    res.set("Content-Type", register.contentType)
-    res.end(await register.metrics())
-    
+	res.send('Hello World!');
+});
+
+app.get('/metrics', async function (req, res) {
+	res.set('Content-Type', register.contentType);
+	res.end(await register.metrics());
 })
 
 app.listen(3000);
